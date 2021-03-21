be43cd68d3b6a4758f6af2de3a18a065a3a9425d
Pavel Pisarev <>
2021-03-21 14:03:58 +0300

    user mapper

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,9 +1,12 @@
 package com.example.demo
 
+import com.example.demo.model.mapper.UserMapper
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 
 @SpringBootTest
+@MockBean(UserMapper::class)
 class DemoApplicationTests {
 
     @Test
